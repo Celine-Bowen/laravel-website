1d8e962d4f0d627b4ee6fa0b3db827fc833d173c
Celine299 <celinebowen55@gmail.com>
2021-01-25 13:03:34 +0300

    first commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+  /*
+  |--------------------------------------------------------------------------
+  | Login Controller
+  |--------------------------------------------------------------------------
+  |
+  | This controller handles authenticating users for the application and
+  | redirecting them to your home screen. The controller uses a trait
+  | to conveniently provide its functionality to your applications.
+  |
+  */
+
+  use AuthenticatesUsers;
+
+  /**
+   * Where to redirect users after login.
+   *
+   * @var string
+   */
+  public function authenticated()
+  {
+    if(auth()->user()->isLead()) {
+      return redirect('/lead/dashboard');
+    } else if(auth()->user()->isFacilitator()) {
+      return redirect('/facilitator/dashboard');
+    } else {
+      return redirect('/dashboard');
+    }
+  }
+
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('guest')->except('logout');
+  }
+}
